8e4c4ae18ad812322d9bb55ec35aff5299a3bf99
garzj <johannes@garz.dev>
2025-04-23 05:35:13 +0200

    chore: upgrade to gnome shell 48 [closes #4]

--- src/tracker/Cursor.ts
+++ src/tracker/Cursor.ts
@@ -1,6 +1,4 @@
 import Clutter from 'gi://Clutter';
-import GLib from 'gi://GLib';
-import Meta from 'gi://Meta';
 import Mtk from 'gi://Mtk';
 import { makeWidget, setStyles } from '../gjs/widget.js';
 import { Shape } from './Shape.js';
@@ -8,31 +6,15 @@ import { Shape } from './Shape.js';
 export class Cursor implements Shape {
   widget = makeWidget();
 
-  private shellTracker = Meta.CursorTracker.get_for_display(global.display);
-
-  private idleSource: undefined | number = undefined;
+  private shellTracker = global.backend.get_cursor_tracker();
 
   constructor() {
     this.shellTracker.connect('visibility-changed', () => this.update());
 
-    this.shellTracker.connect('cursor-changed', () => {
-      this.update();
-
-      // sometimes the correct scale is only returned in next idle phase
-      this.idleSource = GLib.idle_add(GLib.PRIORITY_DEFAULT, () => {
-        this.update();
-        this.idleSource = undefined;
-        return GLib.SOURCE_REMOVE;
-      });
-    });
+    this.shellTracker.connect('cursor-changed', () => this.update());
   }
 
-  destroy() {
-    if (this.idleSource !== undefined) {
-      GLib.source_remove(this.idleSource);
-      this.idleSource = undefined;
-    }
-  }
+  destroy() {}
 
   private update() {
     const texture = this.shellTracker.get_sprite();
